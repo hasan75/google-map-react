e51662ddcc22d6b71759843e5134d32bd393ed8b
Hasan75 <hasanjab14@gmail.com>
2021-10-27 02:33:25 +0600

    google map added for showing in project

---
+++ src/components/Map.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+  
+  const center = {
+    lat: 23.810331,
+    lng: 90.412521
+  };
+
+const Map = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Map;
